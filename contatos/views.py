8c1d93b44542d3c0a64ae6dcf907f76357581f82
PEDRO PAULO DE ABREU <pedro.abreu@alunos.sc.senac.br>
2023-04-27 21:04:19 -0300

    segunda aula

--- contatos/views.py
+++ contatos/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from .models import Contatos
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    contatos = Contatos.objects.all()
+    return render(request, 'pages/index.html', {'contatos': contatos})
+
+
+def search(request):
+    q = request.GET.get('search')
+    contatos = Contatos.objects.filter(nome__icontains=q)
+    return render(request, 'pages/index.html', {'contatos': contatos})

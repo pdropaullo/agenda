f0759ea0d0b424b5c603a486ef4cd0f9d5c311c1
PEDRO PAULO DE ABREU <pedro.abreu@alunos.sc.senac.br>
2023-05-17 21:09:46 -0300

    cadastrar login

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('accounts/login/', views.user_login, name='login'),
-    path('accounts/logout/', views.logout, name='logout')
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('cadastrar/', views.cadastrar, name='cadastrar')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 def user_login(request):
@@ -20,3 +22,17 @@ def user_login(request):
 def logout(request):
     auth.logout(request)
     return redirect('login')
+
+
+def cadastrar(request):
+
+    if request.method == 'POST':
+        # user = request.POST['username']
+        user = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        re_password = request.POST.get('re_password')
+        User.objects.create_user(username=user, email=email, password=password)
+        return redirect('login')
+    else:
+        return render(request, 'pages/cadastrar.html')
